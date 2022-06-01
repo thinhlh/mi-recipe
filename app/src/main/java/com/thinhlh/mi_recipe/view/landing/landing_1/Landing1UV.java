1995a5e4f13d720bfdb18bd2ce153b39c346f657
Hoang Thinh <thinhlh0812@gmail.com>
2022-06-01 11:31:58 +0700

    [FEAT] remove unused files, add themes

---
+++ app/src/main/java/com/thinhlh/mi_recipe/view/landing/landing_1/Landing1UV.java
@@ -0,0 +1,6 @@
+package com.thinhlh.mi_recipe.view.landing.landing_1;
+
+import com.thinhlh.mi_recipe.base.userview.BaseUserView;
+
+public interface Landing1UV extends BaseUserView {
+}
---
+++ app/src/main/java/com/thinhlh/mi_recipe/view/landing/landing_1/Landing1VM.java
@@ -0,0 +1,6 @@
+package com.thinhlh.mi_recipe.view.landing.landing_1;
+
+import com.thinhlh.mi_recipe.base.viewmodel.BaseUiViewModel;
+
+public class Landing1VM extends BaseUiViewModel<Landing1UV> {
+}
